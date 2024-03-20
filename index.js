20fd03f1964a454e3cbee639f8ff25ada9f68f0e
whitney shisia <shisiawhitney215@gmail.com>
2024-03-20 05:20:53 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+      return fare * multiplier;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = (drivers, callback) => {
+    return callback(drivers);
+  };
